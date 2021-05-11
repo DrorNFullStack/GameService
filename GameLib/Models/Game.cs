30ad74601355b697f96cd21d0c9a90a09da63c2e
Dror Nathan <drories@gmail.com>
2021-05-11 16:36:43 +0300

    Adding further functionality

--- GameLib/Models/Game.cs
+++ GameLib/Models/Game.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using GameLib.Inter;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GameLib.Models
 {
-    public class Game
+    public class Game 
     {
+        private readonly IBackgammonLogic _backgammonLogic;
+
+        public Player _playerBlue, _playerRed;
+
+        public int[,] _boardPieces;
+        
         public Guid GameID { get; set; }
+
+        public Game(IBackgammonLogic backgammonLogic)
+        {
+            backgammonLogic = _backgammonLogic;
+        }
+        
+        public int[] RollTheDice(Player roller)
+        {
+            int[] result = _backgammonLogic.DiceRoller();
+
+            return result;
+        }
     }
 }
